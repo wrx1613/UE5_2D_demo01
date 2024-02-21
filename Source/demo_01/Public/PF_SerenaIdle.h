ddc6e2f29745e0d8000f43338dcf42892085bbfc
Wang RuiXiang <wrx1613@163.com>
2024-02-21 22:24:12 +0800

    test

---
+++ Source/demo_01/Public/PF_SerenaIdle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperFlipbookActor.h"
+#include "PF_SerenaIdle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMO_01_API APF_SerenaIdle : public APaperFlipbookActor
+{
+	GENERATED_BODY()
+	
+};
